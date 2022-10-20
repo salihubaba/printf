f90930a3c9a5107cfde2ca553bc2be7d99282d4f
Muhammad Salihu <salihumodibo1@gmail.com>
2022-10-20 03:49:50 -0700

    Fixing

--- main.h
+++ main.h
@@ -1,11 +1,12 @@
-#include <stdlib.h>
 #ifndef MAIN_H
 #define MAIN_H
+
+#include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 
- int _printf(const char *format, ...);
- int _putchar(char c);
+int _printf(const char *format, ...);
+int _putchar(char c);
 
 #endif
